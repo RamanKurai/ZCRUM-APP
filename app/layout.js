560689d534879873622b28b27abae595be607b3f
Raman Kurai <ramankurai27@gmail.com>
2025-01-22 19:10:47 +0530

    Configered Layout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import { Inter }  from "next/font/google"
+import Header from "@/components/header";
 
 const inter = Inter({ subsets : ["latin"]})
 export const metadata = {
@@ -10,16 +11,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className}`}
-      >
-        <ThemeProvider attribute="class" default="dark">
-          {/* header */}
-          {/* main */}
-        {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <html lang="en" suppressHydrationWarning>
+    <body className={`${inter.className} animated-dotted-background`}>
+      <ThemeProvider attribute="class" defaultTheme="dark">
+      <Header/>
+        <main className="min-h-screen">{children}</main>
+        <footer className="bg-gray-900 py-12">
+          <div className="container mx-auto px-4 text-center text-gray-200">
+            <p>Made with 💗 by Raman Kurai</p>
+          </div>
+        </footer>
+      </ThemeProvider>
+    </body>
+  </html>
   );
 }
---
+++ components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      Hlo
+    </div>
+  )
+}
+
+export default Header
