e43225830a5028fb644409893354d94ce0bbf3bc
Raman Kurai <ramankurai27@gmail.com>
2025-01-23 19:54:18 +0530

    error fixing for protectedroute users

--- middleware.js
+++ middleware.js
@@ -11,9 +11,7 @@ const isProtectedRoute = createRouteMatcher([
 export default clerkMiddleware(async (auth, req) => {
   // Restrict admin routes to users with specific permissions
   if (isProtectedRoute(req)) {
-    await auth.protect((has) => {
-      return has()
-    })
+    await auth.protect()
   }
 })
 
