0e61254cb322ceef028f8e3e5b4c66beb63072ff
Raman Kurai <ramankurai27@gmail.com>
2025-01-25 10:19:53 +0530

    add : organization page and orgswitcher

--- actions/organization.js
+++ actions/organization.js
@@ -1,39 +1,48 @@
+"use server";
+
 import { db } from "@/lib/prisma";
 import { auth, clerkClient } from "@clerk/nextjs/server";
 
 export async function getOrganization(slug) {
-    const {userId} = await auth();
-    if (!userId) {
-        throw new Error("Unauthorized");
-    }
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
 
-    const user = await db.user.findUnique({
-      where :{clerkUserId : userId},
-    })
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
 
-    if (!user) {
-        throw new Error("User not found");
-    }
+  if (!user) {
+    throw new Error("User not found");
+  }
 
-    const organization = (await clerkClient()).organizations.getOrganization({
-        slug,
-    });
+  // Get the organization details
+  const organization = await (
+    await clerkClient()
+  ).organizations.getOrganization({
+    slug,
+  });
 
-    if(!organization){
-        return null;
-    }
+  if (!organization) {
+    return null;
+  }
 
-    const {data : membership} = 
-    (await clerkClient()).organizations.getOrganizationMembershipList({
-        organizationId:(await organization).id
-    })
+  // Check if user belongs to this organization
+  const { data: membership } = await (
+    await clerkClient()
+  ).organizations.getOrganizationMembershipList({
+    organizationId: organization.id,
+  });
 
-    const userMembership = membership.find(
-        (member) => member.publicUserData.userId === userId
-    )
+  const userMembership = membership.find(
+    (member) => member.publicUserData.userId === userId
+  );
 
-    if (!userMembership) {
-        return null;
-    }
-    return organization;
+  // If user is not a member, return null
+  if (!userMembership) {
+    return null;
+  }
+
+  return organization;
 }
