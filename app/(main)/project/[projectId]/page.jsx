126ce981bdd0664a4eb7aced5bb20354efeb8782
Raman Kurai <ramankurai27@gmail.com>
2025-01-27 20:45:31 +0530

    Sprint Creation done

--- app/(main)/project/[projectId]/page.jsx
+++ app/(main)/project/[projectId]/page.jsx
@@ -1,17 +1,33 @@
 import { getProject } from '@/actions/projects';
 import { notFound } from 'next/navigation';
 import React from 'react'
+import SprintCreationForm from '../_components/create-sprint';
 
 const ProjectPage = async ({params}) => {
-  const {projectId} = params;
+  const {projectId} =  await params;
   const project = await getProject(projectId)
     if (!project) {
       notFound();
   }
   return (
-    <div>
-      Project Page......
-    </div>
+    <div className="container mx-auto">
+    <SprintCreationForm
+      projectTitle={project.name}
+      projectId={projectId}
+      projectKey={project.key}
+      sprintKey={project.sprints?.length + 1}
+    />
+
+    {project.sprints.length > 0 ? (
+      // <SprintBoard
+      //   sprints={project.sprints}
+      //   projectId={projectId}
+      //   orgId={project.organizationId}
+       <></>
+    ) : (
+      <div>Create a Sprint from button above</div>
+    )}
+  </div>
   )
 }
 
